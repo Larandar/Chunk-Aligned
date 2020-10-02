c0895607522ff751b443db0cd0e38862d26b11c1
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-02 12:54:24 +0200

    ♻️ (Updates): Change way updates are done

--- updates/factorio_extended.lua
+++ updates/factorio_extended.lua
@@ -1,33 +1,46 @@
 -- Factorio-Extented-Plus entities
+local updates = {
+    {
+        type = "electric-pole",
+        name = "medium-electric-pole-mk2",
+        prototypes = {maximum_wire_distance = 32}
+    }, {
+        type = "electric-pole",
+        name = "medium-electric-pole-mk3",
+        prototypes = {maximum_wire_distance = 64}
+    }, {
+        type = "electric-pole",
+        name = "big-electric-pole-mk2",
+        prototypes = {maximum_wire_distance = 48}
+    }, {
+        type = "electric-pole",
+        name = "big-electric-pole-mk3",
+        prototypes = {maximum_wire_distance = 64}
+    }, {
+        type = "electric-pole",
+        name = "substation-mk2",
+        prototypes = {
+            maximum_wire_distance = 48,
+            supply_area_distance = 24,
+            fast_replaceable_group = "big-electric-pole"
+        }
+    }, {
+        type = "electric-pole",
+        name = "substation-mk3",
+        prototypes = {
+            maximum_wire_distance = 64,
+            supply_area_distance = 32,
+            fast_replaceable_group = "big-electric-pole"
+        }
+    }, {
+        type = "roboport",
+        name = "roboport-mk2",
+        prototypes = {logistics_radius = 48, construction_radius = 96}
+    }, {
+        type = "roboport",
+        name = "roboport-mk3",
+        prototypes = {logistics_radius = 64, construction_radius = 128}
+    }
+}
 
-update_data("electric-pole", "medium-electric-pole-mk2", {
-    maximum_wire_distance = 32,
-})
-update_data("electric-pole", "medium-electric-pole-mk3", {
-    maximum_wire_distance = 64,
-})
-update_data("electric-pole", "big-electric-pole-mk2", {
-    maximum_wire_distance = 48,
-})
-update_data("electric-pole", "big-electric-pole-mk3", {
-    maximum_wire_distance = 64,
-})
-update_data("electric-pole", "substation-mk2", {
-    maximum_wire_distance = 48,
-    supply_area_distance = 24,
-    fast_replaceable_group = "big-electric-pole",
-})
-update_data("electric-pole", "substation-mk3", {
-    maximum_wire_distance = 64,
-    supply_area_distance = 32,
-    fast_replaceable_group = "big-electric-pole",
-})
-
-update_data("roboport", "roboport-mk2", {
-    logistics_radius = 48,
-    construction_radius = 96,
-})
-update_data("roboport", "roboport-mk3", {
-    logistics_radius = 64,
-    construction_radius = 128,
-})
+return updates
--- updates/lighted_poles_plus.lua
+++ updates/lighted_poles_plus.lua
@@ -1,13 +1,12 @@
-
-
+-- Dynamicly update all lighted power poles
 for k, _ in pairs(data.raw["electric-pole"]) do
     if k:match("^lighted-") then
-        local origin = data.raw["electric-pole"][k:sub(9,string.len(k))]
+        local origin = data.raw["electric-pole"][k:sub(9, string.len(k))]
         if origin then
             update_data("electric-pole", k, {
                 maximum_wire_distance = origin.maximum_wire_distance,
                 supply_area_distance = origin.supply_area_distance,
-                fast_replaceable_group = origin.fast_replaceable_group,
+                fast_replaceable_group = origin.fast_replaceable_group
             })
         end
     end
