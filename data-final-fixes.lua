9e42870c69737be2c41b004a62406438f1c61455
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-02 12:54:24 +0200

    ♻️ (Updates): Change way updates are done

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,12 +1,22 @@
-function update_data(type, name, prototypes)
+-- Update one prototype only if available
+local function update_data(type, name, prototypes)
     if (data.raw[type] == nil) then return end
     if (data.raw[type][name] == nil) then return end
-    for k, v in pairs(prototypes) do
-        data.raw[type][name][k] = v
+    for k, v in pairs(prototypes) do data.raw[type][name][k] = v end
+end
+
+-- Update multiple prototypes
+local function process_updates(updates)
+    for _, update in ipairs(updates) do
+        update_data(update.type, update.name, update.prototypes)
     end
 end
 
+local vanilla_updates = require("updates.vanilla")
+process_updates(vanilla_updates)
 
-require("updates.vanilla")
-require("updates.factorio_extended")
+local extented_updates = require("updates.factorio_extended")
+process_updates(extented_updates)
+
+-- Update lighted power poles
 require("updates.lighted_poles_plus")
--- updates/vanilla.lua
+++ updates/vanilla.lua
@@ -1,21 +1,30 @@
 -- Vanilla entities
+local updates = {
+    {
+        type = "electric-pole",
+        name = "small-electric-pole",
+        prototypes = {maximum_wire_distance = 8.5}
+    }, {
+        name = "electric-pole",
+        type = "medium-electric-pole",
+        prototypes = {maximum_wire_distance = 16.5}
+    }, {
+        type = "electric-pole",
+        name = "big-electric-pole",
+        prototypes = {maximum_wire_distance = 32}
+    }, {
+        type = "electric-pole",
+        name = "substation",
+        prototypes = {
+            maximum_wire_distance = 32,
+            supply_area_distance = 16,
+            fast_replaceable_group = "big-electric-pole"
+        }
+    }, {
+        type = "roboport",
+        name = "roboport",
+        prototypes = {logistics_radius = 32, construction_radius = 64}
+    }
+}
 
-update_data("electric-pole", "small-electric-pole", {
-    maximum_wire_distance = 8.5,
-})
-update_data("electric-pole", "medium-electric-pole", {
-    maximum_wire_distance = 16.5,
-})
-update_data("electric-pole", "big-electric-pole", {
-    maximum_wire_distance = 32,
-})
-update_data("electric-pole", "substation", {
-    maximum_wire_distance = 32,
-    supply_area_distance = 16,
-    fast_replaceable_group = "big-electric-pole",
-})
-
-update_data("roboport", "roboport", {
-    logistics_radius = 32,
-    construction_radius = 64,
-})
+return updates
