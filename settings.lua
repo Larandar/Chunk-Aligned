d6fc92666981d211a577704bd5797094bc8fdd7e
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-02 14:20:19 +0200

    ✨ (Settings): Add setting for how medium power poles behave

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+    {
+        type = "string-setting",
+        name = "chunk-aligned-strategy",
+        setting_type = "startup",
+        default_value = "Only Substations and Bigs",
+        allowed_values = {
+            "Only Substations and Bigs", "Bigger Mediums", "[DEPRECATED] Legacy"
+        }
+    }
+})
