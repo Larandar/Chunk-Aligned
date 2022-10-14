e6e9c66bb8c55ecfc8808e05189dbfc9a63795b1
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2022-10-14 14:13:49 +0200

    🚑 (Krastorio2): fix statup crash

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,7 +2,10 @@
 local function update_data(type, name, prototypes)
     if (data.raw[type] == nil) then return end
     if (data.raw[type][name] == nil) then return end
-    for k, v in pairs(prototypes) do data.raw[type][name][k] = v end
+    for k, v in pairs(prototypes) do
+        log(string.format("[Chunk-Aligned] Updated %s.%s.%s:  %s -> %s", type, name, k, data.raw[type][name][k], v))
+        data.raw[type][name][k] = v
+    end
 end
 
 -- Update multiple prototypes
@@ -22,6 +25,9 @@ process_updates(vanilla_updates)
 
 local extented_updates = require("updates.factorio_extended")
 process_updates(extented_updates)
+
+local krastorio_updates = require("updates.krastorio2")
+process_updates(krastorio_updates)
 
 -- Update lighted power poles
 require("updates.lighted_poles_plus")
---
+++ updates/krastorio2.lua
@@ -0,0 +1,12 @@
+-- Krastorio2 entities
+local updates = {
+    {
+        type = "electric-pole",
+        name = "kr-substation-mk2",
+        prototypes = {
+            fast_replaceable_group = "electric-pole-2x2"
+        }
+    }
+}
+
+return updates
